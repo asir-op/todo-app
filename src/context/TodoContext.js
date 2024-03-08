c49e51fd3df37ca25343bf5f88ded5013e5669e5
asir-op <asiransari55@gmail.com>
2024-03-08 15:17:38 +0530

    first commit

---
+++ src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "todo here",
+            completed: false,
+        },
+        
+    ],
+    addToDo: (todo) => {},
+    updateTodo: (todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+});
+
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider
